1a0d9271e55af7377545536443c6b2b24c6404a3
Marcus Siegel <msiegel37@gmail.com>
2020-08-18 22:35:51 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,16 +1,44 @@
 require_relative './part_1_solution.rb'
+require 'pry'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  counter = 0 
+  while counter < coupons.length 
+  cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
+  couponed_item_name = 
+  "#{coupons[counter][:item]} W/COUPON"
+  cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
+  #binding.pry
+  if cart_item && cart_item[:count] >= coupons[counter][:num]
+    if cart_item_with_coupon 
+      cart_item_with_coupon[:count] += coupons[counter][:num]
+      cart_item[:count] -= coupons[counter][:num]
+  else cart_item_with_coupon = {
+    :item => couponed_item_name,
+    :price => coupons[counter][:cost] / coupons[counter][:num], 
+    :count => coupons[counter][:num],
+    :clearance => cart_item[:clearance]
+  }
+  cart << cart_item_with_coupon
+  cart_item[:count] -= coupons[counter][:num]
 end
+end
+counter += 1 
+end 
+cart 
+end 
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
-end
+ counter = 0 
+ while counter < cart.length 
+ if cart[counter][:clearance]
+   cart[counter][:price] = 
+   (cart[counter][:price] - (cart[counter][:price] * 0.20)).round(2) 
+  end
+counter += 1
+ end 
+cart 
+end 
 
 def checkout(cart, coupons)
   # Consult README for inputs and outputs
