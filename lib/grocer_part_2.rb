684d156c142f05037644341ae274263612969805
Marcus Siegel <msiegel37@gmail.com>
2020-08-18 22:41:51 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -41,13 +41,17 @@ cart
 end 
 
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
-end
+  total_cart = consolidate_cart(cart)
+  coupon = apply_coupons(consolidate_cart, coupons)
+  final_cart = apply_clearance(coupon)
+  counter = 0 
+  total = 0 
+  while counter < final_cart.length 
+  total += final_cart[counter][:price] * final_cart[counter][:count]
+  counter += 1 
+end 
+if total > 100 
+  total -= (total * 0.10)
+ end 
+total
+end 
